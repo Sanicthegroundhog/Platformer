32dc6bff4cbdcbd353a37a3e95ab0cd3435509a6
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-12-05 13:24:32 -0500

    Initial commit

---
+++ src/main/GameWindow.java
@@ -0,0 +1,38 @@
+package main;
+
+import javax.swing.JFrame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+public class GameWindow {
+    private JFrame jframe;
+
+    public GameWindow(GamePanel gamePanel) {
+
+        jframe = new JFrame();
+
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
+        jframe.setResizable(false);
+        jframe.pack();
+        jframe.setVisible(true);
+
+        WindowFocusListener windowFocusListener = new WindowFocusListener() {
+
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                gamePanel.getGame().windowFocusLost();
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+
+            }
+        };
+
+        jframe.addWindowFocusListener(windowFocusListener);
+
+    }
+
+}
